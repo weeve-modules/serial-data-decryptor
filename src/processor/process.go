f604a144f944953335cb7f7719127732ed80a5b0
shriftz <shrikant.bhusalwad@weeve.network>
2022-09-02 19:10:00 +0530

    added icon and removed unwanted code

--- src/processor/process.go
+++ src/processor/process.go
@@ -44,8 +44,6 @@ func validateData(msg models.Data) error {
 
 func decrypt(msg models.Data) ([]byte, error) {
 	var key = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
-	// log.Infof("AES key %s", utility.GetEnvAsserted("AES_KEY"))
-	// var key = []byte(utility.GetEnvAsserted("AES_KEY"))
 
 	iv, err := base64.StdEncoding.DecodeString(fmt.Sprint(msg["iv"]))
 	if err != nil {
